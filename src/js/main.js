f8361c02477aa5565437ff4768a84ae372e0f88f
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-07-25 00:57:41 +0200

    Add a modal which contains informations of planning

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,18 @@
 import { displayResponsiveNav } from "./util.js";
 import Slider from "./slider.js";
 
-// elements of DOM
+// Display Responsive Main Nav's item
 const $navBar = document.querySelector("#nav-bar");
 const $ulNav = document.querySelector("nav > ul");
+
+displayResponsiveNav($navBar, $ulNav);
+
+// --- Handle slider --- //
 const $slidesImg = document.querySelectorAll(".content-slides > img");
 const $next = document.querySelector(".right");
 const $previous = document.querySelector(".left");
 const $circles = document.querySelectorAll(".circle");
 
-
-// Display Responsive Main Nav's item
-displayResponsiveNav($navBar, $ulNav);
-
-// --- Handle slider --- //
 const slider = new Slider($slidesImg, $next, $previous, $circles, 2);
 
 slider.next.addEventListener("click", slider.nextSlide);
@@ -23,3 +22,16 @@ slider.previous.addEventListener("click", slider.previousSlide);
 document.addEventListener("keydown", slider.keypressed);
 
 slider.handleCircles();
+
+// Manage dialog elements
+const $progBtn = document.querySelector("#programme > button") 
+const $infoProgramme = document.querySelector("#info-programme");
+const $close = document.querySelector("#close");
+
+$progBtn.addEventListener('click', () => {
+    typeof $infoProgramme.showModal === "function" ?
+    $infoProgramme.showModal() :
+    console.error("L'API <dialog> n'est pas prise en charge par ce navigateur.");
+});
+
+$close.addEventListener('click', () => $infoProgramme.close());
