1730f4ba5bdb90078c54cad85c55fde66580ee96
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-08-01 21:54:25 +0200

    Transpile bundle.js with Babel

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,19 @@ module.exports = {
   devServer: {
     contentBase: './dist',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+  }
 };
